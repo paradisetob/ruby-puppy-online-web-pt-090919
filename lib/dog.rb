2cc0b8a065059be8556cca96feb5d5598d1d87b9
tracey brimage <onelovenilynn@gmail.com>
2019-10-08 04:55:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+class dog
+  @@all=[]
+
+  attr_accessor :name
+
+  def initializes(dog.new="snoopy")
+    @dog=dog
+  end
+end
+
+dog.new="pluto"
+dog.new="fido"
+dog.new="maddy"
+
+dog.class_variable_set = dog.all
